610da8a2a949b311ca1def2b7bf0bcb12c73e5ad
Francisco Carmona <flcarmonac@yahoo.com>
2024-06-01 20:04:59 -0400

    Update

---
+++ Proyectos_WebApi.NETCore7/Controllers/ArchivoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Proyectos_WebApi.NETCore7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArchivoController : ControllerBase
+    {
+        private static IWebHostEnvironment _webHostEnvironment;
+
+        public ArchivoController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFile(string fileName)
+        {
+            string path = _webHostEnvironment.WebRootPath + "\\img\\";
+            var filePath = Path.Combine(path, fileName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                byte[] b = System.IO.File.ReadAllBytes(filePath);
+
+                return File(b, "image/webp");
+            }
+
+            return BadRequest(new { Msge = "No Existe el Archivo" });
+        }
+    }
+}
