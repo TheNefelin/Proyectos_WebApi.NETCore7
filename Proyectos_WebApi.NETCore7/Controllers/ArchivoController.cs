9fefc06700e9ea0be76b35c95dd3fa3cf81ac270
Francisco Carmona <flcarmonac@yahoo.com>
2024-06-08 19:33:31 -0400

    Update

--- Proyectos_WebApi.NETCore7/Controllers/ArchivoController.cs
+++ Proyectos_WebApi.NETCore7/Controllers/ArchivoController.cs
@@ -15,9 +15,27 @@ namespace Proyectos_WebApi.NETCore7.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetFile(string fileName)
+        [Route("GuiaJuegos")]
+        public IActionResult GetGuiaJuegosFile(string fileName)
         {
-            string path = _webHostEnvironment.WebRootPath + "\\img\\";
+            string path = _webHostEnvironment.WebRootPath + "\\GuiaJuegos\\";
+            var filePath = Path.Combine(path, fileName);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                byte[] b = System.IO.File.ReadAllBytes(filePath);
+
+                return File(b, "image/webp");
+            }
+
+            return BadRequest(new { Msge = "No Existe el Archivo" });
+        }
+
+        [HttpGet]
+        [Route("F1")]
+        public IActionResult GetF1File(string fileName)
+        {
+            string path = _webHostEnvironment.WebRootPath + "\\F1\\";
             var filePath = Path.Combine(path, fileName);
 
             if (System.IO.File.Exists(filePath))
